fca41aac2e5e8867b39b608650df8eec9aa44dec
Danil Bykon <bykon.danil@gmail.com>
2020-07-16 16:47:34 +0300

    Start create buttun types

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Valve.VR.InteractionSystem;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+    public void OnPress(Hand hand)
+    {
+        Debug.Log("SteamVR Button pressed!");
+    }
+
+    public void OnCustomButtonPress()
+    {
+        Debug.Log("We pushed our custom button!");
+    }
+}
